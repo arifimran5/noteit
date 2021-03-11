f072d682f9834514aef51fec5e46c2b4e7d96f25
Arif Imran <arifimran505@gmail.com>
2021-03-11 16:56:44 +0530

    added title in buttons

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function addNewNote(text = "") {
 
    note.innerHTML = `
     <div class="tools">
-      <button class="edit"><i class="fas fa-edit"></i></button>
-      <button class="delete"><i class="fas fa-trash-alt"></i></button>
+      <button class="edit" title="edit & submit"><i class="fas fa-edit"></i></button>
+      <button class="delete" title="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
 
     <div class="main ${text ? "" : "hidden"}"></div>
